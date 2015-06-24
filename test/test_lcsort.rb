fe0351fb5a3f4ecd2418b0afa6fae882b35aeb42
Jonathan Rochkind <jonathan@dnil.net>
2015-06-24 12:22:32 -0400

    Make test more readable and complete

--- test/test_lcsort.rb
+++ test/test_lcsort.rb
@@ -2,65 +2,54 @@ require 'minitest/autorun'
 require 'lcsort'
 
 class LcsortTest < Minitest::Test
-  
-  TEST_CALLNOS = ['A1', 
-    'B22.3',
-    'C1.D11',
-    'd15.4 .D22 1990',
-    'E8 C11 D22',
-    'ZA4082G33M434.D54 1998',
-    'microfilm'
-  ]
-
-  EXPECTED_NORM = ['A  0001',
-    'B  0022300000',
-    'C  0001000000D110',
-    'D  0015400000D220 000 000 1990',
-    'E  0008000000C110D220',
-    'ZA 4082000000G330M434D540 1998',
-    nil
-  ]
-
-  EXPECTED_ENDRANGE = ['A  0001999999~999~999~999',
-    'B  0022399999~999~999~999',
-    'C  0001000000D119~999~999',
-    'D  0015400000D220 000 000 1990',
-    'E  0008000000C110D229~999',
-    'ZA 4082000000G330M434D540 1998',
-    nil
-  ]
-
-  EXPECTED_WELLFORM = ['A  0001',
-    'B  0022300000',
-    'C  0001000000D110',
-    'D  0015400000D220 000 000 1990',
-    'E  0008000000C110D220',
-    'ZA 4082000000G330M434D540 1998',
-    nil
-  ]
 
   def test_normalization
-    TEST_CALLNOS.each_with_index do |callno, i|
-      assert_equal EXPECTED_NORM[i], Lcsort.normalize(callno)
+    # pairs, left hand normalizes to right-hand
+    [ 
+      ['A1',      'A  0001'],
+      ['B22.3',   'B  0022300000'],
+      ['C1.D11',  'C  0001000000D110'],
+      ['d15.4 .D22 1990', 'D  0015400000D220 000 000 1990'],  
+      ['E8 C11 D22',      'E  0008000000C110D220'],
+      ['ZA4082G33M434.D54 1998', 'ZA 4082000000G330M434D540 1998']
+    ].each do |call, normalized|
+      assert_normalizes_as call, normalized
     end
   end
 
   def test_endrange
-    TEST_CALLNOS.each_with_index do |callno, i|
-      assert_equal EXPECTED_ENDRANGE[i], Lcsort.normalize(callno, :bottomout => true)
-    end
+    # pairs of call number, and expected bottomout/endrange normalized
+    [
+      ['A1',     'A  0001999999~999~999~999'],
+      ['B22.3',  'B  0022399999~999~999~999'],
+      ['C1.D11', 'C  0001000000D119~999~999'],
+      ['d15.4 .D22 1990', 'D  0015400000D220 000 000 1990'],
+      ['E8 C11 D22',      'E  0008000000C110D229~999'], 
+      ['ZA4082G33M434.D54 1998', 'ZA 4082000000G330M434D540 1998']
+    ].each do |call, normalized|
+      assert_normalizes_bottomout_as call, normalized
+    end      
   end
 
-  def test_wellform
-    TEST_CALLNOS.each_with_index do |callno, i|
-      assert_equal EXPECTED_WELLFORM[i], Lcsort.normalize(callno)
-    end
-  end
-
-  def test_bad_callnums
+  def test_non_recognized
+    assert_nil Lcsort.normalize("microform")
     assert_nil Lcsort.normalize("this is not a call number")
     assert_nil Lcsort.normalize("12234")
+    assert_nil Lcsort.normalize("928.12")
   end
+
+  def test_variants
+    # variants that should all normalize the same
+    [
+      ['B 22.3', 'B22.3'],
+      ['C1.D11', 'C 1.D11', 'C 1.D11', 'C 1 .D11'],
+      ['D15.4 .D22 1990', 'D 15.4 .D22 1990', 'D15.4.D22 1990', 'D15.4.D22 1990'],
+      ['E8 C11 D22', 'E8 .C11 .D22', 'E8 .C11.D22', 'E8 .C11D22', 'E8C11D22']
+    ].each do |list|
+      assert_variants_normalize_same(list)
+    end
+  end
+
 
   def test_parses_weird_numbers
     # These may NOT be currently sorted correctly, but they parse somehow,
@@ -68,6 +57,25 @@ class LcsortTest < Minitest::Test
     refute_nil Lcsort.normalize("A1.2 .A54 21st 2010")
     refute_nil Lcsort.normalize("KF 4558 15th .G8")
     refute_nil Lcsort.normalize("JX 45.5 2nd .A54 .G888 2010")
+  end
+
+  def assert_normalizes_as(callno, expected)
+    actual = Lcsort.normalize(callno)
+    assert_equal expected, actual, "Expected `#{callno}` to normalize as `#{expected}` not `#{actual}`"
+  end
+
+  def assert_normalizes_bottomout_as(callno, expected)
+    actual = Lcsort.normalize(callno, :bottomout => true)
+    assert_equal expected, actual, "Expected `#{callno}` to end range normalize as `#{expected}` not `#{actual}`"
+  end
+
+  def assert_variants_normalize_same(list)
+    first_call = list.shift
+
+    list.each do |call|
+      assert_equal Lcsort.normalize(first_call), Lcsort.normalize(call), "Expected `#{call}` to normalize identical to `#{first_call}`"
+      assert_equal Lcsort.normalize(first_call, :bottomout => true), Lcsort.normalize(call, :bottomout => true), "Expected `#{call}` to bottomout/endrange normalize identical to `#{first_call}`"
+    end
   end
 
 end
